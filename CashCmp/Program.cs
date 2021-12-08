9efa639f804fa57c55b011a18ae295e7e3fcd7db
Sandun <sandunfx@gmail.com>
2021-12-08 09:36:37 +0530

    print

--- CashCmp/Program.cs
+++ CashCmp/Program.cs
@@ -5,18 +5,64 @@ using Newtonsoft.Json.Linq;
 using System.Globalization;
 
 var cash = ReadCash();
-var tr = ReadTr();
-var ez = ReadEx();
+var bill = ReadBill();
+var ez = ReadEz();
 
 foreach (var item in cash)
 {
     JObject o = JObject.Parse(item.Data);
-} 
+
+    var type = o["tr"].ToString();
+
+    if (type.Contains("paybill") || type.Contains("datapay"))
+    {
+        string accNo = o["con"][0]["n"].Value<string>();
+
+        if(accNo.StartsWith('0'))
+        {
+            accNo = accNo.Substring(1);
+        }
+
+        var qu = bill.Where(t => t.Account == accNo)
+                     .Select(t => t.Account)
+                     .FirstOrDefault();
+
+        if (qu == null)
+        {
+            Console.WriteLine("0b- " + item.Date + "  " + o.ToString());
+            Console.WriteLine("--------------------------------------");
+        }
+    }
+    else if (type.Contains("ez"))
+    {
+        var accNo = o["con"].Value<string>();
+
+        if (accNo.StartsWith('0'))
+        {
+            accNo = accNo.Substring(1);
+        }
+
+        var qu = ez.Where(t => t.Account == accNo)
+                   .Select(t => t.Account)
+                   .FirstOrDefault();
+        if (qu == null)
+        { 
+            Console.WriteLine("0e- " + item.Date + "  " + o.ToString());
+            Console.WriteLine("--------------------------------------");
+        }
+
+    }
+    else
+    {
+        Console.WriteLine("0e- " + item.Date + "  " + o.ToString());
+        Console.WriteLine("--------------------------------------");
+    }
+}
 
 Console.WriteLine("P");
 Console.Read();
 
-static List<Tr> ReadTr() 
+static List<Tr> ReadBill() 
 {
     using (var reader = new StreamReader(@"D:\c3\dialogkiosk6\test\CashCmpF\bill.csv"))
     using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
@@ -26,7 +72,7 @@ static List<Tr> ReadTr()
     }
 }
 
-static List<Ez> ReadEx()
+static List<Ez> ReadEz()
 {
     using (var reader = new StreamReader(@"D:\c3\dialogkiosk6\test\CashCmpF\ez.csv"))
     using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
